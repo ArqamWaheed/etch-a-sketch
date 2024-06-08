b27b8baf9bf2f4bcfce93c49ffe10b9da18b700d
ArqamWaheed <86839084+ArqamWaheed@users.noreply.github.com>
2024-06-08 11:05:17 +0500

    Added a reset button and a select grid option.

--- javascript.js
+++ javascript.js
@@ -1,16 +1,42 @@
 const container = document.querySelector(".container");
-let size = 900/16;
+const reset = document.querySelector("#reset");
+const dimensions = document.querySelector("#dimensions");
 
-for (let i = 1; i <= 16*16; i++) {
-    const div = document.createElement("div");
-    div.style.cssText = `outline: 2px solid black; height: ${size}px; width: ${size}px;`;
-    container.appendChild(div);
+let grids = 16; // 16 x 16 grid by default
+generateGrid()
+
+
+function generateGrid() { // Creates the grid and adds the sketching functionality
+    let size = 900/grids;
+    for (let i = 1; i <= grids*grids; i++) {
+        const div = document.createElement("div");
+        div.style.cssText = `outline: 2px solid black; height: ${size}px; width: ${size}px; background-color:white;`;
+        container.appendChild(div);
+    }
+    const div = document.querySelectorAll(".container div")
+    div.forEach((element) => {
+        element.addEventListener("mouseover", (event) => {
+            event.target.style.backgroundColor = "blue";
+        })
+    })
 }
 
-const div = document.querySelectorAll(".container div")
-
-div.forEach((element) => {
-    element.addEventListener("mouseover", (event) => {
-        event.target.style.backgroundColor = "blue";
+reset.addEventListener("click", () => { // Resets the grid.
+    const div = document.querySelectorAll(".container div")
+    div.forEach((element) => {
+        element.style.backgroundColor = "white";
     })
+})
+
+dimensions.addEventListener("click", () => { // Sets the grid dimensions.
+    let value = parseInt(prompt("Select your grid, can be from 10x10 to 100x100. Enter ONLY n for a n x n grid."));
+    if (value >= 10 && value <= 100) grids = value;
+    else {
+        alert("WRONG INPUT! Should only be an integer within range 10 to 100");
+    }
+    const div = document.querySelectorAll(".container div")
+    div.forEach((element) => {
+        container.removeChild(element);
+    })
+    generateGrid()
 })
